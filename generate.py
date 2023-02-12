5a585a3c040772ed4e85184c839cdb7c321cecfb
Benjamin Flippy <agentflippy5@users.noreply.github.com>
2023-02-12 18:55:57 -0500

    Restructure and improvements
    First commit since fork
    -Fixed bug that downloads the same hash file over and over
    -Changed method to use status code to identify end of available hashes

--- generate.py
+++ generate.py
@@ -8,26 +8,20 @@ with open('virushashes.txt', 'w') as virushashes:
 	for id in range(99999):
 		url = f'https://virusshare.com/hashfiles/VirusShare_{str(id).zfill(5)}.md5'
 
-		try:
-			response = requests.get('https://virusshare.com/hashfiles/VirusShare_00000.md5', stream=True)
-			response.encoding = 'utf-8'
-
+		response = requests.get(url, stream=True)
+		response.encoding = 'utf-8'
+		if response.status_code != 200:
+			print("Reached end of files.")
+			break
+		else:
 			for line in response.iter_lines(decode_unicode=True):
 				if line.startswith('#'):
 					continue
-
 				if isFirstLine:
 					isFirstLine = False
 				else:
 					line = '\n' + line
-
 				virushashes.write(line)
-
-			print(f'Downloaded {url}')	
-		except requests.exceptions.HTTPError as error:
-			if error.response.status_code == 404:
-				break
-
-			raise error
+			print(f'Downloaded {url}')
 
 print('Download complete')
